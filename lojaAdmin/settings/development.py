957e9c754516f1a8198bf00b8e3f7387e463d469
lucas-albuq <luuinhasalbuquerque@gmail.com>
2024-03-08 10:23:06 -0300

    Configuração de ambiente

---
+++ lojaAdmin/settings/development.py
@@ -0,0 +1,11 @@
+from .settings import*
+DEBUG = True
+#Crie secret key para seu ambiente de desenvolvimento
+SECRET_KEY='ixb62ha#ts=ab4t2u%p1_62-!5w2j==j6d^3-j$!z(@*m+-h'
+ALLOWED_HOSTS = ['127.0.0.1']
+DATABASES={
+'default':{
+'ENGINE':'django.db.backends.sqlite3',
+'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
+}
---
+++ lojaAdmin/settings/production.py
@@ -0,0 +1,11 @@
+from .settings import *
+DEBUG = False
+#Crie a secret key para seu ambiente de produção
+SECRET_KEY = 'ixb6fha#ts=&b4t2u%p1_62-!8dw2j==j)d^3-j$!z(@*m+-h'
+ALLOWED_HOSTS = ['127.0.0.1']
+DATABASES = {
+'default':{
+'ENGINE':'django.db.backends.sqlite3',
+'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
+}
