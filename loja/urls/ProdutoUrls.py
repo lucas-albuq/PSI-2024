e784d65616592629ffe045cdb49108548b954d2a
Lucas Albuquerque <luuinhasalbuquerque@gmail.com>
2024-08-08 16:12:09 -0300

    até criar produto, mas não tá salvando, ver isso dps

--- loja/urls/ProdutoUrls.py
+++ loja/urls/ProdutoUrls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from loja.views.ProdutoView import list_produto_view, edit_produto_view
+from loja.views.ProdutoView import list_produto_view, edit_produto_view, edit_produto_postback, details_produto_view, delete_produto_view, delete_produto_postback, create_produto_view
 
 urlpatterns = [
     path("", list_produto_view, name= 'produto'),
     path("<int:id>", list_produto_view, name= 'produto'),
     path("edit/<int:id>", edit_produto_view, name= 'edit_produto'),
+    path("edit", edit_produto_postback, name= 'edit_produto_postback'),
+    path("details/<int:id>", details_produto_view, name= 'details_produto'),
+    path("delete/<int:id>", delete_produto_view, name='delete_produto'),
+    path("delete", delete_produto_postback, name='delete_produto_postback'),
+    path("create", create_produto_view, name= 'create_produto'),
 ]
--- loja/views/ProdutoView.py
+++ loja/views/ProdutoView.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
+from django.core.files.storage import FileSystemStorage
+
 def list_produto_view(request, id=None):
     produto = request.GET.get("produto")
     destaque = request.GET.get("destaque")
@@ -30,7 +32,7 @@ def list_produto_view(request, id=None):
     print(produtos)
     context = {
         'produtos': produtos
-    }
+        }
     return render(request, template_name='produto/produto.html', context=context, status=200)
 
 def edit_produto_view(request, id=None):
@@ -45,14 +47,13 @@ def edit_produto_view(request, id=None):
     return render(request, template_name='produto/produto-edit.html', context=context,status=200)
 
 def edit_produto_postback(request, id=None):
-# Processa o post back gerado pela action
     if request.method == 'POST':
         id = request.POST.get("id")
         produto = request.POST.get("Produto")
         destaque = request.POST.get("destaque")
         promocao = request.POST.get("promocao")
         msgPromocao = request.POST.get("msgPromocao")
-        
+
         print("postback")
         print(id)
         print(produto)
@@ -73,3 +74,81 @@ def edit_produto_postback(request, id=None):
         except Exception as e:
             print("Erro salvando edição de produto: %s" % e)
     return redirect("/produto")
+
+def details_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = {
+        'produto': produto
+        }
+    return render(request, template_name='produto/produto-details.html', context=context, status=200)
+
+def delete_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = {
+        'produto': produto
+        }
+    return render(request, template_name='produto/produto-delete.html', context=context, status=200)
+
+def delete_produto_postback(request, id=None):
+    if request.method == 'POST':
+        id = request.POST.get("id")
+        produto = request.POST.get("Produto")
+        print("postback-delete")
+        print(id)
+        try:
+            Produto.objects.filter(id=id).delete()
+            print("Produto %s excluido com sucesso" % produto)
+        except Exception as e:
+            print("Erro salvando edição de produto: %s" % e)
+    return redirect("/produto")
+
+def create_produto_view(request, id=None):
+    if request.method == 'POST':
+        produto = request.POST.get("Produto")
+        destaque = request.POST.get("destaque")
+        promocao = request.POST.get("promocao")
+        msgPromocao = request.POST.get("msgPromocao")
+        preco = request.POST.get("preco")
+        image = request.POST.get("image")
+        print("postback-create")
+        print(produto)
+        print(destaque)
+        print(promocao)
+        print(msgPromocao)
+        print(preco)
+        print(image)
+        try:
+            obj_produto = Produto()
+            obj_produto.Produto = produto
+            obj_produto.destaque = (destaque is not None)
+            obj_produto.promocao = (promocao is not None)
+            if msgPromocao is not None:
+                obj_produto.msgPromocao = msgPromocao
+                obj_produto.preco = 0
+            if (preco is not None) and ( preco != ""):
+                obj_produto.preco = preco
+            obj_produto.criado_em = timezone.now()
+            obj_produto.alterado_em = obj_produto.criado_em
+            if request.FILES is not None:
+                num_files = len(request.FILES.getlist('image'))
+                if num_files > 0:
+                    imagefile = request.FILES['image']
+                    print(imagefile)
+                    fs = FileSystemStorage()
+                    filename = fs.save(imagefile.name, imagefile)
+                    if (filename is not None) and (filename != ""):
+                        obj_produto.image = filename
+            obj_produto.save()
+            print("Produto %s salvo com sucesso" % produto)
+        except Exception as e:
+            print("Erro inserindo produto: %s" % e)
+        return redirect("/produto")
+    return render(request, template_name='produto/produto-create.html',status=200)
