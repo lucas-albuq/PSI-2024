ab28a690d32862594f8bc626b62e94388377980e
Lucas Albuquerque <luuinhasalbuquerque@gmail.com>
2024-08-08 11:47:08 -0300

    Cap 6

--- loja/views/ProdutoView.py
+++ loja/views/ProdutoView.py
@@ -1,6 +1,30 @@
 from django.http import HttpResponse
-
+from loja.models import Produto
+from datetime import timedelta, datetime
+from django.utils import timezone
 def list_produto_view(request, id=None):
-    if id is None:
-        id = 0
-    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+    produto = request.GET.get("produto")
+    destaque = request.GET.get("destaque")
+    promocao = request.GET.get("promocao")
+    categoria = request.GET.get("categoria")
+    fabricante = request.GET.get("fabricante")
+    dias = request.GET.get("dias")
+    produtos = Produto.objects.filter(Produto=produto)
+    produtos = Produto.objects.all()
+    if dias is not None:
+        now = timezone.now()
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gte=now)
+    if produto is not None:
+        produtos = produtos.filter(Produto__contains=produto)
+    if promocao is not None:
+        produtos = produtos.filter(promocao=promocao)
+    if destaque is not None:
+        produtos = produtos.filter(destaque=destaque)
+    if categoria is not None:
+        produtos = produtos.filter(categoria__Categoria=categoria)
+    if fabricante is not None:
+        produtos = produtos.filter(fabricante__Fabricante=fabricante)
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    return HttpResponse('<h1>Produto de id %s!</h1>' % id) 
